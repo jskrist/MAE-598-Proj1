75af332f5b7333fcc2c083a70193fd501caaaff5
James Kristoff <jskrist@gmail.com>
2011-10-15 15:36:05 -0700

    added dynamic arrays and re-tooled to reduce memory usage

--- proj1_E.cpp
+++ proj1_E.cpp
@@ -3,14 +3,16 @@
 #include <math.h>
 #include <mpi.h>
 
-double update(int &x,int &y,int &z);
+double update(int &x,int &y,int &z, double &nodes);
+double updateFace(int &x, int &y, int &z, double &nodes, Face &fFace);
 void   setDims(int &nSize);
 void   setNeighbors();
 //**************************************************
 //  Global Variables
 //**************************************************
 
-// the step size in each direction and default Temp
+// 1 over the step size in each direction 
+// and default Temp
 const double dw = 200, // X
 	   		 dh = 200, // Y
 	    	 dl = 200, // Z
@@ -27,10 +29,10 @@ const int i = (int)(w*dw),
 		  k = (int)(l*dl);
 
 // Array of all the nodes
-double nodes[k][j][i] = { defltTemp };
+//double nodes[k][j][i] = { defltTemp };
 
 // Thermal defusivity and time step
-double alpha=1, dt=0.00003;
+double alpha=1, dt=1E-5;
 
 // Cartesian communicator variables
 MPI_Comm oldComm, cartComm;
@@ -40,8 +42,10 @@ int ndims = 3,
 	reorder = 0,
 	coords[3];
 
+// processor rank and group size
 int rank, size;
 
+// flags to check neighbors existence
 bool left	= false,
 	 right	= false,
 	 top	= false,
@@ -49,12 +53,16 @@ bool left	= false,
 	 front	= false,
 	 back	= false;
 
+// rank of neighbors will be stored here
 int  nLeft		= -1,
 	 nRight		= -1,
 	 nTop		= -1,
 	 nBottom	= -1,
 	 nFront		= -1,
 	 nBack		= -1;
+
+// Enumerated type to send to updateFace to know what face to update
+enum Face { LEFT, RIGHT, TOP, BOTTOM, FRONT, BACK };
 
 //**************************************************
 //  Main Program
@@ -74,6 +82,38 @@ int main(int argc, char** argv)
 		dY = j / dims[1],
 		dZ = k / dims[2];
 	
+	// Dynamically create a 3d array to store values
+	double ***core; // = (double***)malloc(( dX-2 )*sizeof(double**));
+	core = new double**[dZ-2];
+	for (int r = 0; r < dZ-2; ++r)
+	{
+		core[r] = new double*[dY-2];
+
+		for (int q = 0; q < dY-2; ++q)
+ 			core[r][q] = new double[dX-2];
+	}
+	
+/*	for (int r = 0; r < (dZ-2); r++)
+	{
+    	core[r] = (double**)malloc((dZ-2)*sizeof(double*));
+    	for (int q = 0; q < (dY-2); q++)
+		{
+      		core[r][q] = (double*)malloc((dY-2)*sizeof(double));
+    	}
+  	}
+*/
+	// initialize core array
+	for(int r = 0; r < dZ-2; r++)
+	{
+		for(int q = 0; q < dY-2; q++)
+		{
+			for(int p = 0; p < dX-2; p++)
+			{
+				core[r][q][p] = 0;
+			}
+		}
+	}
+
 	// Creates a cartesian communicator
 	MPI_Cart_create(oldComm, ndims, dims, 
 					periods, reorder, &cartComm);
@@ -86,10 +126,12 @@ int main(int argc, char** argv)
 	nLoopY = coords[1] * dY;
 	nLoopZ = coords[2] * dZ;
 
-//	setNeihbors();
+	// Determine what neighbors are around
+	setNeihbors();
 
 	// Open file to which data will be written
 	FILE *Ofile;
+
 	// Only have the first processor control the file
 	if(rank == size - 1)
 	{
@@ -97,12 +139,12 @@ int main(int argc, char** argv)
 	}
 
 	// coordinate of the middle of the structure
-	int midI = (int)floor(double(i) * 0.5);
-	int midJ = (int)floor(double(j) * 0.5);
+	//	int midI = (int)floor(double(i) * 0.5);
+	//	int midJ = (int)floor(double(j) * 0.5);
 
-	int cnt = 0;
-	double qo = 100.0;
-	double qf = 0.0;
+	int cnt = 0;		// loop counter
+	double qo = 100.0;	// initial front temp
+	double qf = 0.0;	// initial back temp
 
 	double	lastTemp = defltTemp,
 			diff = 0,
@@ -115,128 +157,216 @@ int main(int argc, char** argv)
 	int  done 	 = 0,
 		 changed = 0;
 
+	// Dynamically create send data arrays
+	double** sendLeft;
+	double** sendRight;
+	double** sendTop;
+	double** sendBottom;
+	double** sendFront;
+	double** sendBack;
+
+	// Dynamically create recv data array
+	double** recvLeft;
+	double** recvRight;
+	double** recvTop;
+	double** recvBottom;
+	double** recvFront;
+	double** recvBack;
+
+	sendLeft 	= new double*[dZ];
+	sendRight	= new double*[dZ];
+	for (int r = 0; r < dZ; ++r)
+	{
+    	sendLeft[r]  = new double[dY];
+    	sendRight[r] = new double[dY];
+	}
+
+	sendTop		= new double*[dZ];
+	sendBottom	= new double*[dZ];
+	for (int r = 0; r < dZ; ++r)
+	{
+    	sendTop[r]    = new double[dX];
+    	sendBottom[r] = new double[dX];
+	}
+
+	sendFront 	= new double*[dY];
+	sendBack	= new double*[dY];
+	for (int q = 0; q < dY; ++q)
+	{
+    	sendFront[q] = new double[dX];
+    	sendBack[q]	 = new double[dX];
+	}
+
+/*	// Dynamically create send data arrays
+	double* sendLeft	= (double*) malloc(dY * dZ * sizeof(double));
+	double* sendRight	= (double*) malloc(dY * dZ * sizeof(double));
+	double* sendTop		= (double*) malloc(dX * dZ * sizeof(double));
+	double* sendBottom	= (double*) malloc(dX * dZ * sizeof(double));
+	double* sendFront	= (double*) malloc(dY * dX * sizeof(double));
+	double* sendBack	= (double*) malloc(dY * dX * sizeof(double));
+
+	// Dynamically create recv data array
+	double* recvLeft	= (double*) malloc(dY * dZ * sizeof(double));
+	double* recvRight	= (double*) malloc(dY * dZ * sizeof(double));
+	double* recvTop		= (double*) malloc(dX * dZ * sizeof(double));
+	double* recvBottom	= (double*) malloc(dX * dZ * sizeof(double));
+	double* recvFront	= (double*) malloc(dY * dX * sizeof(double));
+	double* recvBack	= (double*) malloc(dY * dX * sizeof(double));
+*/
+
+	for(int z = 0; z < dZ; z++)
+	{
+		for(int y = 0; y < dY; y++)
+		{
+			sendLeft[z][y]  = 0;
+			sendRight[z][y] = 0;
+			recvLeft[z][y]  = 0;
+			recvRight[z][y] = 0;
+		}
+	}
+	for(int z = 0; z < dZ; z++)
+	{
+		for(int x = 0; x < dX; x++)
+		{
+			sendTop[z][x]	 = 0;
+			sendBottom[z][x] = 0;
+			recvTop[z][x]	 = 0;
+			recvBottom[z][x] = 0;
+		}
+	}
+	for(int y = 0; y < dY; y++)
+	{
+		for(int x = 0; x < dX; x++)
+		{
+			sendFront[y][x]	= 0;
+			sendBack[y][x]	= 0;
+			recvFront[y][x]	= 0;
+			recvBack[y][x]	= 0;
+		}
+	}
+
 	// Set initial temperature
 	if(nLoopZ == 0)
 	{
-		for(int y = nLoopY; y < nLoopY + dY; y++)
+		for(int y = 0; y < dY; y++)
 		{
-			for(int x = nLoopX; x < nLoopX + dX; x++)
+			for(int x = 0; x < dX; x++)
 			{
-				nodes[0][y][x] = qo;
+				sendFront[y][x] = qo;
 			}
 		}
 	}
 	else if (nLoopZ + dZ == k)
 	{
-		for(int y = nLoopY; y < nLoopY + dY; y++)
+		for(int y = 0; y < dY; y++)
 		{
-			for(int x = nLoopX; x < nLoopX + dX; x++)
+			for(int x = 0; x < dX; x++)
 			{
-				nodes[k-1][y][x] = qf;
+				sendBack[y][x] = qf;
 			}
 		}
 	}
 
 	// Make sure all of the initalization is done
-//	MPI_Barrier(cartComm);
-	MPI_Allreduce( MPI_IN_PLACE, &nodes, i*j*k, MPI_DOUBLE, MPI_MAX, cartComm );
-
-/*	// Dynamically create data arrays
-	if(left)
-	{
-		double* vLeft	= (double*) malloc(nLoopY * nLoopZ * sizeof(double));
-		for(int z = nLoopZ; z < nLoopZ + dZ; z++)
-		{
-			for(int y = nLoopY; y < nLoopY + dY; y++)
-			{
-				vLeft[z - nLoopZ][y - nLoopY] = nodes[z][y][nLoopX];
-			}
-		}
-	}
-	if(right)
-	{
-		double* vRight	= (double*) malloc(nLoopY * nLoopZ * sizeof(double));
-		for(int z = nLoopZ; z < nLoopZ + dZ; z++)
-		{
-			for(int y = nLoopY; y < nLoopY + dY; y++)
-			{
-				vLeft[z - nLoopZ][y - nLoopY] = nodes[z][y][nLoopX + dX];
-			}
-		}
-	}
-	if (top)
-	{
-		double* vTop	= (double*) malloc(nLoopX * nLoopZ * sizeof(double));
-	}
-	if (bottom)
-	{
-		double* vBottom	= (double*) malloc(nLoopX * nLoopZ * sizeof(double));
-	}
-	if (front)
-	{
-		double* vFront	= (double*) malloc(nLoopX * nLoopY * sizeof(double));
-	}
-	if (back)
-	{
-		double* vBack	= (double*) malloc(nLoopX * nLoopY * sizeof(double));
-	}
-*/
-
+	// MPI_Allreduce( MPI_IN_PLACE, &nodes, i*j*k, MPI_DOUBLE, MPI_MAX, cartComm );
 
 	while(done == 0)
 	{
 //std::cout << rank << " is still working\n";
 		cnt++;
-		for(int z = nLoopZ; z < nLoopZ + dZ; z++)
+		for(int z = 0; z < dZ-2; z++)
 		{
-			if(z != 0 && z != k-1)
+			for(int y = 0; y < dY-2; y++)
 			{
-				for(int y = nLoopY; y < nLoopY + dY; y++)
+				for(int x = 0; x < dX-2; x++)
 				{
-					for(int x = nLoopX; x < nLoopX + dX; x++)
+					// Update core 3D array
+					curNodeTemp = updateCore(x, y, z, core);
+					// Update each face
+					updateFace(x, y, z, sendLeft, LEFT);
+					updateFace(x, y, z, sendRight, RIGHT);
+					updateFace(x, y, z, sendTop, TOP);
+					updateFace(x, y, z, sendBottom, BOTTOM);
+					// Only update front and back if they are not
+					// the very front or very back
+					if(nLoopZ != 0)
+						updateFace(x, y, z, sendFront, FRONT);
+					if(nLoopZ + dZ != k)
+						updateFace(x, y, z, sendBack, BACK);
+
+					core[z][y][x] = curNodeTemp;
+
+					if(curNodeTemp > (qo + qf))
 					{
-						curNodeTemp = update(x,y,z);
-						nodes[z][y][x] = curNodeTemp;
+						printf("Became unstable");
+						MPI_Finalize();
 
-						if(curNodeTemp > (qo + qf))
-						{
-							printf("Became unstable");
-							MPI_Finalize();
-
-							return 1;
-						}
-						else if(curNodeTemp < (defltTemp + minDiff) )
-						{
-							endLoop = true;
-							break;
-						}
+						return 1;
 					}
-					if(endLoop)
+					else if(curNodeTemp < (defltTemp + minDiff) )
+					{
+						endLoop = true;
 						break;
+					}
 				}
 				if(endLoop)
 					break;
 			}
+			if(endLoop)
+				break;
 		}
 		endLoop = false;
 
-		if( changed == 0 )
+		// Update each face
+		for(int z = 0; z < dZ; z++)
 		{
-			if(nodes[k-2][nLoopY][nLoopX] > defltTemp + minChg || nodes[k-2][nLoopY][nLoopX] < defltTemp - minChg)
+			for(int y = 0; y < dY; y++)
 			{
-				changed = 1;
+				updateFace(0, y, z, sendLeft, LEFT);
+				updateFace(dX, y, z, sendRight, RIGHT);
 			}
 		}
-		//  ASUMPTION that the loop in which it initially changes is not the same 
-		//  loop that it reaches the steady-state otherwise we will get one more
-		//  itteration
-		else
+		for(int z = 0; z < dZ; z++)
 		{
-			diff = fabs(lastTemp - nodes[k-2][nLoopY][nLoopX]);
-			lastTemp = nodes[k-2][nLoopY][nLoopX];
-			if(diff < minDiff)
+			for(int x = 0; x < dX; x++)
 			{
-				done = 1;
+				updateFace(x, dY, z, sendTop, TOP);
+				updateFace(x, 0, z, sendBottom, BOTTOM);
+			}
+		}
+		for(int y = 0; y < dY; y++)
+		{
+			for(int x = 0; x < dX; x++)
+			{
+				// Only update front and back if they are not
+				// the very front or very back
+				if(nLoopZ != 0)
+					updateFace(x, y, 0, sendFront, FRONT);
+				if(nLoopZ + dZ != k)
+					updateFace(x, y, dZ, sendBack, BACK);
+			}
+		}
+
+		if(nLoopZ + dZ == k)
+		{
+			if( changed == 0 )
+			{
+				if(sendBack[0][0] > defltTemp + minChg || sendBack[0][0] < defltTemp - minChg)
+				{
+					changed = 1;
+				}
+			}
+			//  ASUMPTION that the loop in which it initially changes is not the same 
+			//  loop that it reaches the steady-state otherwise we will get one more
+			//  itteration
+			else
+			{
+				diff = fabs(lastTemp - sendBack[0][0]);
+				lastTemp = sendBack[0][0];
+				if(diff < minDiff)
+				{
+					done = 1;
+				}
 			}
 		}
 
@@ -245,12 +375,9 @@ int main(int argc, char** argv)
 			// If one processor meets the ending requirements then send that to
 			// all processors so they can quit working
 			MPI_Allreduce( MPI_IN_PLACE, &done, 1, MPI_INT, MPI_MAX, cartComm);
-
-			// Make sure all data is up to date at the end of every 100th time step
-			MPI_Allreduce( MPI_IN_PLACE, &nodes, i*j*k, MPI_DOUBLE, MPI_MAX, cartComm);
-			MPI_Allreduce( MPI_IN_PLACE, &changed, 1, MPI_INT, MPI_MAX, cartComm);	
 		}
 	}
+
 	// Note when a processor exits the loop
 	// std::cout << "Node " << rank << " is out\n";
 	
@@ -263,8 +390,55 @@ int main(int argc, char** argv)
 		}
 		fclose(Ofile);
 	}
-	// Make sure all the nodes are ready before we finalize and exit
-	//MPI_Barrier(oldComm);
+
+/*	for(int r = 0; r < dZ-2; r++)
+	{
+		for(int q = 0; q < dY-2; q++)
+		{
+			for(int p = 0; p < dX-2; p++)
+			{
+				free(core[r][q][p]);
+			}
+		}
+	}
+	free(core);
+*/
+	// Release the allocated Memory
+	for (int q = 0; q < dY; ++q)
+	{
+		delete [] sendLeft[r][q];
+		delete [] sendRight[r][q];
+	}
+
+	for (int p = 0; p < dX; ++p)
+	{
+		delete [] sendTop[r][p];
+		delete [] sendBottom[r][p];
+	}
+
+	for (int p = 0; p < dX; ++p)
+	{
+		delete [] sendFront[q][p];
+		delete [] sendBack[q][p];
+	}
+
+	for (int q = 0; q < dY; ++q)
+	{
+		for (int p = 0; p < dX; ++p)
+		{
+			delete [] core[q][p];
+		}
+
+		delete [] core[q];
+	}
+
+	delete [] core;
+	delete [] sendLeft;
+	delete [] sendRight;
+	delete [] sendTop;
+	delete [] sendBottom;
+	delete [] sendFront;
+	delete [] sendBottom;
 
 	MPI_Finalize();
 	return 0;
@@ -319,7 +493,49 @@ void setDims(int &nSize)
 	dims[2] = tmpDim;	
 }
 
-double update(int &x, int &y, int &z)
+double update(int &x, int &y, int &z, double &nodes)
+{
+	double result = 0;
+
+	double xP1=0,
+		   xM1=0,
+		   yP1=0,
+		   yM1=0,
+		   zP1=0,
+		   zM1=0,
+		   nCur=nodes[z][y][x];
+
+	if(x+1 < i)
+		xP1=nodes[z][y][x+1];
+	else
+		xP1=nCur;
+
+	if(x-1 >= 0)
+		xM1=nodes[z][y][x-1];
+	else
+		xM1=nCur;
+
+	if(y+1 < j)
+		yP1=nodes[z][y+1][x];
+	else
+		yP1=nCur;
+
+	if(y-1 >= 0)
+		yM1=nodes[z][y-1][x];
+	else
+		yM1=nCur;
+
+	zP1=nodes[z+1][y][x];
+	zM1=nodes[z-1][y][x];
+
+	result = alpha*dt*(pow((double)dw,2)*(xM1-2*nCur+xP1)
+					 + pow((double)dh,2)*(yM1-2*nCur+yP1)
+					 + pow((double)dl,2)*(zM1-2*nCur+zP1)) + nCur;
+
+	return result;
+}
+
+double updateFace(int &x, int &y, int &z, double &nodes, Face &fFace);
 {
 	double result = 0;
 
