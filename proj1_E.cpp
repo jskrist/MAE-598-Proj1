89c528bb57a48abd0b162074f169be0df3ce9d3c
James Kristoff <jskrist@gmail.com>
2011-10-16 00:33:57 -0700

    update face function added

--- proj1_E.cpp
+++ proj1_E.cpp
@@ -3,8 +3,12 @@
 #include <math.h>
 #include <mpi.h>
 
-double update(int &x,int &y,int &z, double &nodes);
-double updateFace(int &x, int &y, int &z, double &nodes, Face &fFace);
+// Enumerated type to send to updateFace to know what face to update
+enum Face { LEFT, RIGHT, TOP, BOTTOM, FRONT, BACK };
+
+double updateFace(int &x, int &y, int &z, Face fFace);
+double updateCore(int &x, int &y, int &z);
+
 void   setDims(int &nSize);
 void   setNeighbors();
 //**************************************************
@@ -13,9 +17,9 @@ void   setNeighbors();
 
 // 1 over the step size in each direction 
 // and default Temp
-const double dw = 200, // X
-	   		 dh = 200, // Y
-	    	 dl = 200, // Z
+const double dw = 20, // X
+	   		 dh = 20, // Y
+	    	 dl = 20, // Z
 			 defltTemp = 0;
 
 // Physical Dimensions of the box
@@ -27,9 +31,6 @@ const double w = 2, // X
 const int i = (int)(w*dw),
 	  	  j = (int)(h*dh),
 		  k = (int)(l*dl);
-
-// Array of all the nodes
-//double nodes[k][j][i] = { defltTemp };
 
 // Thermal defusivity and time step
 double alpha=1, dt=1E-5;
@@ -61,8 +62,22 @@ int  nLeft		= -1,
 	 nFront		= -1,
 	 nBack		= -1;
 
-// Enumerated type to send to updateFace to know what face to update
-enum Face { LEFT, RIGHT, TOP, BOTTOM, FRONT, BACK };
+double ***core;
+// Dynamically create send data arrays
+double** sendLeft;
+double** sendRight;
+double** sendTop;
+double** sendBottom;
+double** sendFront;
+double** sendBack;
+
+// Dynamically create recv data array
+double** recvLeft;
+double** recvRight;
+double** recvTop;
+double** recvBottom;
+double** recvFront;
+double** recvBack;
 
 //**************************************************
 //  Main Program
@@ -76,14 +91,25 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+	// Creates a cartesian communicator
+	MPI_Cart_create(oldComm, ndims, dims, 
+					periods, reorder, &cartComm);
+
+	// Get the coordinates of this processor
+	MPI_Cart_coords(cartComm, rank, 3, coords);
+
 	setDims(size);
 
 	int dX = i / dims[0],
 		dY = j / dims[1],
 		dZ = k / dims[2];
-	
+
+	// Set the start points for the loops
+	nLoopX = coords[0] * dX;
+	nLoopY = coords[1] * dY;
+	nLoopZ = coords[2] * dZ;
+
 	// Dynamically create a 3d array to store values
-	double ***core; // = (double***)malloc(( dX-2 )*sizeof(double**));
 	core = new double**[dZ-2];
 	for (int r = 0; r < dZ-2; ++r)
 	{
@@ -93,15 +119,6 @@ int main(int argc, char** argv)
  			core[r][q] = new double[dX-2];
 	}
 	
-/*	for (int r = 0; r < (dZ-2); r++)
-	{
-    	core[r] = (double**)malloc((dZ-2)*sizeof(double*));
-    	for (int q = 0; q < (dY-2); q++)
-		{
-      		core[r][q] = (double*)malloc((dY-2)*sizeof(double));
-    	}
-  	}
-*/
 	// initialize core array
 	for(int r = 0; r < dZ-2; r++)
 	{
@@ -114,20 +131,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-	// Creates a cartesian communicator
-	MPI_Cart_create(oldComm, ndims, dims, 
-					periods, reorder, &cartComm);
-
-	// Get the coordinates of this processor
-	MPI_Cart_coords(cartComm, rank, 3, coords);
-
-	// Set the start points for the loops
-	nLoopX = coords[0] * dX;
-	nLoopY = coords[1] * dY;
-	nLoopZ = coords[2] * dZ;
-
 	// Determine what neighbors are around
-	setNeihbors();
+	setNeighbors();
 
 	// Open file to which data will be written
 	FILE *Ofile;
@@ -137,10 +142,6 @@ int main(int argc, char** argv)
 	{
 		Ofile = fopen("Data.txt","w");
 	}
-
-	// coordinate of the middle of the structure
-	//	int midI = (int)floor(double(i) * 0.5);
-	//	int midJ = (int)floor(double(j) * 0.5);
 
 	int cnt = 0;		// loop counter
 	double qo = 100.0;	// initial front temp
@@ -157,22 +158,7 @@ int main(int argc, char** argv)
 	int  done 	 = 0,
 		 changed = 0;
 
-	// Dynamically create send data arrays
-	double** sendLeft;
-	double** sendRight;
-	double** sendTop;
-	double** sendBottom;
-	double** sendFront;
-	double** sendBack;
-
-	// Dynamically create recv data array
-	double** recvLeft;
-	double** recvRight;
-	double** recvTop;
-	double** recvBottom;
-	double** recvFront;
-	double** recvBack;
-
+	// Dynamically set array sizes
 	sendLeft 	= new double*[dZ];
 	sendRight	= new double*[dZ];
 	for (int r = 0; r < dZ; ++r)
@@ -197,23 +183,7 @@ int main(int argc, char** argv)
     	sendBack[q]	 = new double[dX];
 	}
 
-/*	// Dynamically create send data arrays
-	double* sendLeft	= (double*) malloc(dY * dZ * sizeof(double));
-	double* sendRight	= (double*) malloc(dY * dZ * sizeof(double));
-	double* sendTop		= (double*) malloc(dX * dZ * sizeof(double));
-	double* sendBottom	= (double*) malloc(dX * dZ * sizeof(double));
-	double* sendFront	= (double*) malloc(dY * dX * sizeof(double));
-	double* sendBack	= (double*) malloc(dY * dX * sizeof(double));
-
-	// Dynamically create recv data array
-	double* recvLeft	= (double*) malloc(dY * dZ * sizeof(double));
-	double* recvRight	= (double*) malloc(dY * dZ * sizeof(double));
-	double* recvTop		= (double*) malloc(dX * dZ * sizeof(double));
-	double* recvBottom	= (double*) malloc(dX * dZ * sizeof(double));
-	double* recvFront	= (double*) malloc(dY * dX * sizeof(double));
-	double* recvBack	= (double*) malloc(dY * dX * sizeof(double));
-*/
-
+	// Initialize face arrays
 	for(int z = 0; z < dZ; z++)
 	{
 		for(int y = 0; y < dY; y++)
@@ -267,9 +237,7 @@ int main(int argc, char** argv)
 		}
 	}
 
-	// Make sure all of the initalization is done
-	// MPI_Allreduce( MPI_IN_PLACE, &nodes, i*j*k, MPI_DOUBLE, MPI_MAX, cartComm );
-
+	int zero = 0;
 	while(done == 0)
 	{
 //std::cout << rank << " is still working\n";
@@ -281,19 +249,7 @@ int main(int argc, char** argv)
 				for(int x = 0; x < dX-2; x++)
 				{
 					// Update core 3D array
-					curNodeTemp = updateCore(x, y, z, core);
-					// Update each face
-					updateFace(x, y, z, sendLeft, LEFT);
-					updateFace(x, y, z, sendRight, RIGHT);
-					updateFace(x, y, z, sendTop, TOP);
-					updateFace(x, y, z, sendBottom, BOTTOM);
-					// Only update front and back if they are not
-					// the very front or very back
-					if(nLoopZ != 0)
-						updateFace(x, y, z, sendFront, FRONT);
-					if(nLoopZ + dZ != k)
-						updateFace(x, y, z, sendBack, BACK);
-
+					curNodeTemp = updateCore(x, y, z);
 					core[z][y][x] = curNodeTemp;
 
 					if(curNodeTemp > (qo + qf))
@@ -322,16 +278,16 @@ int main(int argc, char** argv)
 		{
 			for(int y = 0; y < dY; y++)
 			{
-				updateFace(0, y, z, sendLeft, LEFT);
-				updateFace(dX, y, z, sendRight, RIGHT);
+				updateFace(zero, y, z, LEFT);
+				updateFace(dX, y, z, RIGHT);
 			}
 		}
 		for(int z = 0; z < dZ; z++)
 		{
 			for(int x = 0; x < dX; x++)
 			{
-				updateFace(x, dY, z, sendTop, TOP);
-				updateFace(x, 0, z, sendBottom, BOTTOM);
+				updateFace(x, dY, z, TOP);
+				updateFace(x, zero, z, BOTTOM);
 			}
 		}
 		for(int y = 0; y < dY; y++)
@@ -341,9 +297,9 @@ int main(int argc, char** argv)
 				// Only update front and back if they are not
 				// the very front or very back
 				if(nLoopZ != 0)
-					updateFace(x, y, 0, sendFront, FRONT);
+					updateFace(x, y, zero, FRONT);
 				if(nLoopZ + dZ != k)
-					updateFace(x, y, dZ, sendBack, BACK);
+					updateFace(x, y, dZ, BACK);
 			}
 		}
 
@@ -384,42 +340,30 @@ int main(int argc, char** argv)
 	// When the last node exits have it write out to the data file
 	if(rank == size - 1)
 	{
-		for(int z = 0; z < k; z++)
+		for(int z = 0; z < dZ; z++)
 		{
-			fprintf(Ofile,"%i,%f,%i\n", cnt, nodes[z][midJ][midI], rank);
+			fprintf(Ofile,"%i,%f\n", cnt, core[z][0][0]);
 		}
 		fclose(Ofile);
 	}
 
-/*	for(int r = 0; r < dZ-2; r++)
-	{
-		for(int q = 0; q < dY-2; q++)
-		{
-			for(int p = 0; p < dX-2; p++)
-			{
-				free(core[r][q][p]);
-			}
-		}
-	}
-	free(core);
-*/
 	// Release the allocated Memory
+	for (int r = 0; r < dZ; ++r)
+	{
+		delete [] sendLeft[r];
+		delete [] sendRight[r];
+	}
+
+	for (int r = 0; r < dZ; ++r)
+	{
+		delete [] sendTop[r];
+		delete [] sendBottom[r];
+	}
+
 	for (int q = 0; q < dY; ++q)
 	{
-		delete [] sendLeft[r][q];
-		delete [] sendRight[r][q];
-	}
-
-	for (int p = 0; p < dX; ++p)
-	{
-		delete [] sendTop[r][p];
-		delete [] sendBottom[r][p];
-	}
-
-	for (int p = 0; p < dX; ++p)
-	{
-		delete [] sendFront[q][p];
-		delete [] sendBack[q][p];
+		delete [] sendFront[q];
+		delete [] sendBack[q];
 	}
 
 	for (int q = 0; q < dY; ++q)
@@ -447,32 +391,32 @@ int main(int argc, char** argv)
 void setNeighbors()
 {
 	// left
-	MPI_Cart_shift(cartComm, 0, -1, rank, nLeft);
+	MPI_Cart_shift(cartComm, 0, -1, &rank, &nLeft);
 	if(nLeft >=0 && nLeft <=size)
 		left = true;
 
 	// right
-	MPI_Cart_shift(cartComm, 0, 1, rank, nRight);
+	MPI_Cart_shift(cartComm, 0, 1, &rank, &nRight);
 	if(nRight >=0 && nRight <=size)
 		right = true;
 
 	// top
-	MPI_Cart_shift(cartComm, 1, 1, rank, nTop);
+	MPI_Cart_shift(cartComm, 1, 1, &rank, &nTop);
 	if(nTop >=0 && nTop <=size)
 		top = true;
 
 	// bottom
-	MPI_Cart_shift(cartComm, 1, -1, rank, nBottom);
+	MPI_Cart_shift(cartComm, 1, -1, &rank, &nBottom);
 	if(nBottom >=0 && nBottom <=size)
 		bottom = true;
 
 	// front
-	MPI_Cart_shift(cartComm, 2, -1, rank, nFront);
+	MPI_Cart_shift(cartComm, 2, -1, &rank, &nFront);
 	if(nFront >=0 && nFront <=size)
 		front = true;
 
 	// back
-	MPI_Cart_shift(cartComm, 2, 1, rank, nBack);
+	MPI_Cart_shift(cartComm, 2, 1, &rank, &nBack);
 	if(nBack >=0 && nBack <=size)
 		back = true;
 }
@@ -493,7 +437,7 @@ void setDims(int &nSize)
 	dims[2] = tmpDim;	
 }
 
-double update(int &x, int &y, int &z, double &nodes)
+double updateCore(int &x, int &y, int &z)
 {
 	double result = 0;
 
@@ -503,30 +447,91 @@ double update(int &x, int &y, int &z, double &nodes)
 		   yM1=0,
 		   zP1=0,
 		   zM1=0,
-		   nCur=nodes[z][y][x];
+		   nCur=core[z][y][x];
 
-	if(x+1 < i)
-		xP1=nodes[z][y][x+1];
+	if(x+1 < dX-1)
+		xP1=core[z][y][x+1];
 	else
-		xP1=nCur;
+	{
+		if(right)
+		{
+			xP1=sendRight[z+1][y+1];
+		}
+		else
+		{
+			xP1=nCur;
+		}
+	}
 
 	if(x-1 >= 0)
-		xM1=nodes[z][y][x-1];
+		xM1=core[z][y][x-1];
 	else
-		xM1=nCur;
+	{
+		if(left)
+		{
+			xM1=sendLeft[z+1][y+1];
+		}
+		else
+		{
+			xM1=nCur;
+		}
+	}
 
-	if(y+1 < j)
-		yP1=nodes[z][y+1][x];
+	if(y+1 < dY-1)
+		yP1=core[z][y+1][x];
 	else
-		yP1=nCur;
+	{
+		if(top)
+		{
+			yP1=sendTop[z+1][x+1];
+		}
+		else
+		{
+			yP1=nCur;
+		}
+	}
 
 	if(y-1 >= 0)
-		yM1=nodes[z][y-1][x];
+		yM1=core[z][y-1][x];
 	else
-		yM1=nCur;
+	{
+		if(bottom)
+		{
+			yM1=sendBottom[z+1][x+1];
+		}
+		else
+		{
+			yM1=nCur;
+		}
+	}
 
-	zP1=nodes[z+1][y][x];
-	zM1=nodes[z-1][y][x];
+	if(z+1 < dZ-1)
+		zP1=core[z+1][y][x];
+	else
+	{
+		if(back)
+		{
+			zP1=sendBack[y+1][x+1];
+		}
+		else
+		{
+			zP1=nCur;
+		}
+	}
+
+	if(z-1 >= 0)
+		zM1=core[z-1][y][x];
+	else
+	{
+		if(front)
+		{
+			zM1=sendFront[y+1][x+1];
+		}
+		else
+		{
+			zM1=nCur;
+		}
+	}
 
 	result = alpha*dt*(pow((double)dw,2)*(xM1-2*nCur+xP1)
 					 + pow((double)dh,2)*(yM1-2*nCur+yP1)
@@ -535,7 +540,7 @@ double update(int &x, int &y, int &z, double &nodes)
 	return result;
 }
 
-double updateFace(int &x, int &y, int &z, double &nodes, Face &fFace);
+double updateFace(int &x, int &y, int &z, Face fFace)
 {
 	double result = 0;
 
@@ -545,30 +550,438 @@ double updateFace(int &x, int &y, int &z, double &nodes, Face &fFace);
 		   yM1=0,
 		   zP1=0,
 		   zM1=0,
-		   nCur=nodes[z][y][x];
+		   nCur=0;
 
-	if(x+1 < i)
-		xP1=nodes[z][y][x+1];
-	else
-		xP1=nCur;
+	if( fFace == LEFT)
+	{
+		nCur = sendLeft[z][y];
+		if(y > 0 && y < dY && z > 0 && z < dZ)
+			xP1=core[z-1][y-1][0];
+		else if(y == 0)
+			xP1=sendBottom[z][x];
+		else if (y == dY-1)
+			xP1=sendTop[z][x];
+		else if(z == 0)
+			xP1=sendFront[y][x];
+		else if (z == dZ-1)
+			xP1=sendBack[y][x];
+		xM1=recvLeft[z][y];
 
-	if(x-1 >= 0)
-		xM1=nodes[z][y][x-1];
-	else
-		xM1=nCur;
+		if(y+1 < dY)
+			yP1=sendLeft[z][y+1];
+		else
+		{
+			if(top)
+			{
+				yP1=recvTop[z][x];
+			}
+			else
+			{
+				yP1=nCur;
+			}
+		}
 
-	if(y+1 < j)
-		yP1=nodes[z][y+1][x];
-	else
-		yP1=nCur;
+		if(y-1 >= 0)
+			yM1=sendLeft[z][y-1];
+		else
+		{
+			if(bottom)
+			{
+				yM1=recvBottom[z][x];
+			}
+			else
+			{
+				yM1=nCur;
+			}
+		}
 
-	if(y-1 >= 0)
-		yM1=nodes[z][y-1][x];
-	else
-		yM1=nCur;
+		if(z+1 < dZ)
+			zP1=sendLeft[z+1][y];
+		else
+		{
+			if(back)
+			{
+				zM1=recvBack[y][x];
+			}
+			else
+			{
+				zM1=nCur;
+			}
+		}
 
-	zP1=nodes[z+1][y][x];
-	zM1=nodes[z-1][y][x];
+		if(z-1 >= 0)
+			zM1=sendLeft[z-1][y];
+		else
+		{
+			if(front)
+			{
+				zP1=recvFront[y][x];
+			}
+			else
+			{
+				zP1=nCur;
+			}
+		}
+	}
+	else if( fFace == RIGHT)
+	{
+		nCur = sendRight[z][y];
+		xP1=recvRight[z][y];
+		if(y > 0 && y < dY && z > 0 && z < dZ)
+			xM1=core[z-1][y-1][dX-2];
+		else if(y == 0)
+			xM1=sendBottom[z][x];
+		else if (y == dY-1)
+			xM1=sendTop[z][x];
+		else if(z == 0)
+			xM1=sendFront[y][x];
+		else if (z == dZ-1)
+			xM1=sendBack[y][x];
+
+		if(y+1 < dY)
+			yP1=sendRight[z][y+1];
+		else
+		{
+			if(top)
+			{
+				yP1=recvTop[z][x];
+			}
+			else
+			{
+				yP1=nCur;
+			}
+		}
+
+		if(y-1 >= 0)
+			yM1=sendRight[z][y-1];
+		else
+		{
+			if(bottom)
+			{
+				yM1=recvBottom[z][x];
+			}
+			else
+			{
+				yM1=nCur;
+			}
+		}
+
+		if(z+1 < dZ)
+			zP1=sendRight[z+1][y];
+		else
+		{
+			if(back)
+			{
+				zM1=recvBack[y][x];
+			}
+			else
+			{
+				zM1=nCur;
+			}
+		}
+
+		if(z-1 >= 0)
+			zM1=sendRight[z-1][y];
+		else
+		{
+			if(front)
+			{
+				zP1=recvFront[y][x];
+			}
+			else
+			{
+				zP1=nCur;
+			}
+		}
+	}
+	else if( fFace == TOP)
+	{
+		nCur = sendTop[z][x];
+
+		if(x+1 < dX)
+			xP1=sendTop[z][x+1];
+		else
+		{
+			if(right)
+			{
+				xP1=recvRight[z][y];
+			}
+			else
+			{
+				xP1=nCur;
+			}
+		}
+
+		if(x-1 >= 0)
+			xM1=sendTop[z][x-1];
+		else
+		{
+			if(left)
+			{
+				xM1=recvLeft[z][y];
+			}
+			else
+			{
+				xM1=nCur;
+			}
+		}
+
+		yP1=recvTop[z][x];
+		if(x > 0 && x < dX && z > 0 && z < dZ)
+			yM1=core[z-1][dY-2][x-1];
+		else if(x == 0)
+			yM1=sendLeft[z][y];
+		else if (x == dX-1)
+			yM1=sendRight[z][y];
+		else if(z == 0)
+			yM1=sendFront[y][x];
+		else if (z == dZ-1)
+			yM1=sendBack[y][x];
+
+		if(z+1 < dZ)
+			zP1=sendTop[z+1][y];
+		else
+		{
+			if(back)
+			{
+				zP1=recvBack[y][x];
+			}
+			else
+			{
+				zP1=nCur;
+			}
+		}
+
+		if(z-1 >= 0)
+			zM1=sendRight[z-1][y];
+		else
+		{
+			if(front)
+			{
+				zM1=recvFront[y][x];
+			}
+			else
+			{
+				zM1=nCur;
+			}
+		}
+	}
+	else if( fFace == BOTTOM)
+	{
+		nCur = sendBottom[z][x];
+
+		if(x+1 < dX)
+			xP1=sendBottom[z][x+1];
+		else
+		{
+			if(right)
+			{
+				xP1=recvRight[z][y];
+			}
+			else
+			{
+				xP1=nCur;
+			}
+		}
+
+		if(x-1 >= 0)
+			xM1=sendBottom[z][x-1];
+		else
+		{
+			if(left)
+			{
+				xM1=recvLeft[z][y];
+			}
+			else
+			{
+				xM1=nCur;
+			}
+		}
+
+		yM1=recvBottom[z][x];
+		if(x > 0 && x < dX && z > 0 && z < dZ)
+			yP1=core[z-1][0][x-1];
+		else if(x == 0)
+			yP1=sendLeft[z][y];
+		else if (x == dX-1)
+			yP1=sendRight[z][y];
+		else if(z == 0)
+			yP1=sendFront[y][x];
+		else if (z == dZ-1)
+			yP1=sendBack[y][x];
+
+		if(z+1 < dZ)
+			zP1=sendBottom[z+1][y];
+		else
+		{
+			if(back)
+			{
+				zP1=recvBack[y][x];
+			}
+			else
+			{
+				zP1=nCur;
+			}
+		}
+
+		if(z-1 >= 0)
+			zM1=sendRight[z-1][y];
+		else
+		{
+			if(front)
+			{
+				zM1=recvFront[y][x];
+			}
+			else
+			{
+				zM1=nCur;
+			}
+		}
+	}
+	else if( fFace == FRONT)
+	{
+		nCur = sendFront[z][x];
+
+		if(x+1 < dX)
+			xP1=sendFront[z][x+1];
+		else
+		{
+			if(right)
+			{
+				xP1=recvRight[z][y];
+			}
+			else
+			{
+				xP1=nCur;
+			}
+		}
+
+		if(x-1 >= 0)
+			xM1=sendFront[z][x-1];
+		else
+		{
+			if(left)
+			{
+				xM1=recvLeft[z][y];
+			}
+			else
+			{
+				xM1=nCur;
+			}
+		}
+
+		if(y+1 < dY)
+			yP1=sendFront[z][y+1];
+		else
+		{
+			if(top)
+			{
+				yP1=recvTop[z][x];
+			}
+			else
+			{
+				yP1=nCur;
+			}
+		}
+
+		if(y-1 >= 0)
+			yM1=sendFront[z][y-1];
+		else
+		{
+			if(bottom)
+			{
+				yM1=recvBottom[z][x];
+			}
+			else
+			{
+				yM1=nCur;
+			}
+		}
+
+		zM1=recvFront[y][x];
+		if(x > 0 && x < dX && y > 0 && y < dY)
+			zP1=core[dZ-2][y-1][x-1];
+		else if(x == 0)
+			zP1=sendLeft[z][y];
+		else if (x == dX-1)
+			zP1=sendRight[z][y];
+		else if(y == 0)
+			zP1=sendBottom[z][x];
+		else if (y == dY-1)
+			zP1=sendTop[z][x];
+	}
+	else if( fFace == BACK)
+	{
+		nCur = sendBack[z][x];
+
+		if(x+1 < dX)
+			xP1=sendBack[z][x+1];
+		else
+		{
+			if(right)
+			{
+				xP1=recvRight[z][y];
+			}
+			else
+			{
+				xP1=nCur;
+			}
+		}
+
+		if(x-1 >= 0)
+			xM1=sendFront[z][x-1];
+		else
+		{
+			if(left)
+			{
+				xM1=recvLeft[z][y];
+			}
+			else
+			{
+				xM1=nCur;
+			}
+		}
+
+		if(y+1 < dY)
+			yP1=sendFront[z][y+1];
+		else
+		{
+			if(top)
+			{
+				yP1=recvTop[z][x];
+			}
+			else
+			{
+				yP1=nCur;
+			}
+		}
+
+		if(y-1 >= 0)
+			yM1=sendFront[z][y-1];
+		else
+		{
+			if(bottom)
+			{
+				yM1=recvBottom[z][x];
+			}
+			else
+			{
+				yM1=nCur;
+			}
+		}
+
+		zP1=recvBack[y][x];
+		if(x > 0 && x < dX && y > 0 && y < dY)
+			zM1=core[dZ-2][y-1][x-1];
+		else if(x == 0)
+			zM1=sendLeft[z][y];
+		else if (x == dX-1)
+			zM1=sendRight[z][y];
+		else if(y == 0)
+			zM1=sendBottom[z][x];
+		else if (y == dY-1)
+			zM1=sendTop[z][x];
+	}
 
 	result = alpha*dt*(pow((double)dw,2)*(xM1-2*nCur+xP1)
 					 + pow((double)dh,2)*(yM1-2*nCur+yP1)
